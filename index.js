80e51570a9f51ff1664bc9a72fe8b1e3bae41c51
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2024-07-07 14:37:26 +0100

    set up buyWeapons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let xp = 0;
 let health = 100;
 let gold = 50;
-let currentWeaponIndex = 0;
+let currentWeaponIndex = 0; //The value of the currentWeaponIndex variable corresponds to an index in the weapons array. The player starts with a "stick", since currentWeaponIndex starts at 0 and weapons[0] is the "stick" weapon.
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
@@ -17,7 +17,27 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-// hold different locations like store, cave and town square [as objects]
+// hold weapons [as objects - in an array]
+const weapons = [
+  {
+    name: "stick", //The value of the currentWeaponIndex variable corresponds to an index in the weapons array. The player starts with a "stick", since currentWeaponIndex starts at 0 and weapons[0] is the "stick" weapon.
+    power: "5",
+  },
+  {
+    name: "dagger",
+    power: "30",
+  },
+  {
+    name: "claw hammer",
+    power: "50",
+  },
+  {
+    name: "sword",
+    power: "100",
+  },
+];
+
+// hold different locations like store, cave and town square [as objects - in an array]
 const locations = [
   {
     name: "town square",
@@ -86,7 +106,30 @@ function buyHealth() {
   }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  // Check if the currentWeaponIndex is less than the index of the last weapon
+  if (currentWeaponIndex < weapons.length - 1) {
+    //array indexing starts at zero, the index of the last element in an array is one less than the length of the array. Therefore, the condition should check if currentWeaponIndex is less than weapons.length - 1. {
+    //actions the player takes
+    if (gold >= 30) {
+      gold -= 30; // gold equal to 30 less than its current value
+      currentWeaponIndex++; // add 1 to currentWeaponIndex - the user is buying the next weapon in the weapons array
+      goldText.innerText = gold; //update new value of gold
+      let newWeapon = weapons[currentWeaponIndex].name; //tell the player what weapon they bought
+      text.innerText = "You now have a " + newWeapon + "."; //update text
+      inventory.push(newWeapon); //Add the newWeapon to the end of the inventory array
+      text.innerText += " In your inventory you have: " + inventory; //use the += operator to add text to the end of text.innerText
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {}
 
 function fightSlime() {}
 
